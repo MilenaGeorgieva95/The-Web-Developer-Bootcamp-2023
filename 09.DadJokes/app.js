efebf7f347003ea823d44ba0a6b8be607151a262
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2023-08-08 15:08:04 +0100

    Add files via upload

---
+++ 09.DadJokes/app.js
@@ -0,0 +1,21 @@
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
+  } catch (e) {
+    return "NO JOKES AVAILABLE! SORRY :(";
+  }
+};
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+};
+
+button.addEventListener("click", addNewJoke);
