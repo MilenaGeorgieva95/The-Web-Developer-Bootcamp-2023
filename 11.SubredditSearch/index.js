f89b8865eec77881e4486edcd3d6162b1a381d39
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2023-10-17 15:14:39 +0100

    Add files via upload

---
+++ 11.SubredditSearch/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!");
+});
